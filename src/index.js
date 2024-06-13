b0f9e4c537157fb687aa980095f02b9280dfaa3b
Hybola <keeratipong.b@gmail.com>
2024-06-13 16:21:03 +0700

    StarRating component with generic reusable API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App-redo.js";
+// import "./index.css";
+// import App from "./App.js";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+    <StarRating />
+    <StarRating
+      size={24}
+      color="red"
+      className="test"
+      initialRating={3}
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <Test />
   </React.StrictMode>
 );
